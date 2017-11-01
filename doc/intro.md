e095aafeae4c273ff087d82494333386ba510c25
apeckham <apeckham@gmail.com>
2017-10-31 21:55:06 -0700

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to liquid-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/liquid_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns liquid-clj.core-test
+  (:require [clojure.test :refer :all]
+            [liquid-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
