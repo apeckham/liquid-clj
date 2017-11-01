83dd27cbe85642ec19761d513599a7c1fd290c77
apeckham <apeckham@gmail.com>
2017-10-31 22:16:10 -0700

    tdd!!

--- test/liquid_clj/core_test.clj
+++ test/liquid_clj/core_test.clj
@@ -1,7 +1,33 @@
 (ns liquid-clj.core-test
   (:require [clojure.test :refer :all]
-            [liquid-clj.core :refer :all]))
+            [liquid-clj.core :refer :all]
+            [zweikopf.core :as z]
+            [hiccup.core :refer [html]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(z/init-ruby-context)
+(z/ruby-require "liquid")
+
+#_(defn -main
+  [& args]
+  (z/init-ruby-context)
+  (z/ruby-require "liquid")
+
+  #_(-> "Liquid::Template.parse(\"hi {{name}}\").render('name' => 'world')"
+        z/ruby-eval
+        prn)
+
+  (let [template (html [:span {:class "foo"} "Hello " (liquid-var :name)])]
+    (-> "Liquid::Template"
+        z/ruby-eval
+        (z/call-ruby :parse template)
+        (z/call-ruby :render (z/rubyize {"name" "world!"}))
+        prn)))
+
+(defn render [template data]
+  (-> "Liquid::Template"
+      z/ruby-eval
+      (z/call-ruby :parse template)
+      (z/call-ruby :render (z/rubyize data))))
+
+(deftest render-test
+  (is (= "hello world!" (render "hello {{name}}!" {"name" "world"}))))
