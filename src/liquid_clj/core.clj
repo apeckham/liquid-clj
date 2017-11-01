605ea09c01f1389088bce94e4d0f20ed0cf2d541
apeckham <apeckham@gmail.com>
2017-10-31 22:00:17 -0700

    run liquid in clojure!

--- src/liquid_clj/core.clj
+++ src/liquid_clj/core.clj
@@ -1,7 +1,11 @@
 (ns liquid-clj.core
-  (:gen-class))
+  (:gen-class)
+  (:require [zweikopf.core :refer [init-ruby-context]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (z/init-ruby-context)
+  (z/require "liquid")
+  (prn (z/ruby-eval "Liquid::Template.parse(\"hi {{name}}\").render('name' => 'world')"))
   (println "Hello, World!"))
